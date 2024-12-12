c732b45b9f94c57cfc4499868041811f4f31f933
Emiliano Manganiello <emiliano.manganiello@flockit.com.ar>
2024-12-12 14:05:25 -0300

    poniendole href al logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -11,7 +11,9 @@ interface LogoProps {
 export const Logo: FC<LogoProps> = ({ src, width, height }) => {
   return (
     <div className="self-start relative left-[6.5%] mt-[5%] flex">
-      <img src={src} alt="Logo" style={{ width, height }} />
+      <a href="/">
+        <img src={src} alt="Logo" style={{ width, height }} />
+      </a>
     </div>
   );
 };
